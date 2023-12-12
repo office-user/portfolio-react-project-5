84414ddcf022c9f455021e12a1fddbc0c27ec418
office-user <153056648+office-user@users.noreply.github.com>
2023-12-12 18:53:48 +0530

    Create a header and style them.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,33 @@
 
 const Header = () => {
   return (
-    <div>header</div>
+    <>
+    <nav>
+        <NavContent />
+    </nav>
+    </>
   )
 }
+
+
+const NavContent =()=>{
+    return(
+        <>
+        <h1>Gagan</h1>
+        <div>
+            <a href="#home">Home</a>
+            <a href="#word">Word</a>
+            <a href="#experience">Experience</a>
+            <a href="#testimonial">Testimonial</a>
+            <a href="#contact">Contact</a>
+        </div>
+        <a href="mailto:gagan@gmail.com"><button>Email</button></a>
+
+        </>
+    );
+}
+
+
+
 
 export default Header;
