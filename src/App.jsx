196380ce77817a4e2372d953ae2a124001a8c887
office-user <153056648+office-user@users.noreply.github.com>
2023-12-13 15:04:02 +0530

    Add home page and style them

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 // Import Components
 import Header from "./components/Header.jsx";
+import Home from "./pages/Home.jsx";
 // Import Style
-import "./styles/app.scss"
-
+import "./styles/app.scss";
 
 function App() {
   return (
     <>
-    <Header />
-      <div>app</div>
+      <Header />
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,86 @@
+import { motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
+import { BsArrowUpRight } from "react-icons/bs";
+import profileimage  from "../assets/logo.png"
+
+const Home = () => {
+  // with  object
+  const amimations = {
+    h1: {
+      initial: {
+        x: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        x: 0,
+        opacity: 1,
+      },
+    },
+
+    button: {
+      initial: {
+        y: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        y: 0,
+        opacity: 1,
+      },
+    },
+  };
+
+  return (
+    <div id="home">
+      <section>
+        <div>
+          <motion.h1 {...amimations.h1}>
+            Hi I Am <br /> Gagan Tirpude
+          </motion.h1>
+
+          <Typewriter
+            options={{
+              strings: ["A Developer", "A Designer", "A Creator"],
+              autoStart: true,
+              loop: true,
+              wrapperClassName: "typewriterpara",
+              //   cursor: "",
+            }}
+          />
+
+          <div>
+            <a href="mailto:gagan@gmail.com">Here Me</a>
+            <a href="#work">
+              Projects <BsArrowUpRight />
+            </a>
+          </div>
+
+          <article>
+            <p>
+              +<span>100</span>
+            </p>
+            <span>Clients Worldwide</span>
+          </article>
+
+          <aside>
+            <article>
+              <p>
+                +<span>500</span>
+              </p>
+              <span>Project Made</span>
+            </article>
+
+            <article data-special>
+              <p>Contact</p>
+              <span>officail_gagan@gmail.com</span>
+            </article>
+          </aside>
+        </div>
+      </section>
+      <section>
+        <img src={profileimage} alt="gagan" />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
